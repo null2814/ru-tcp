4f8ea832b1d6606ae2d884ea1cac74eae40979fc
lzy <zongyu.lang@weimob.com>
2022-07-06 19:54:34 +0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:7878")?;
+
+    for stream in listener.incoming() {
+        handle_connection(stream?)
+    }
+
+    Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+
+    stream.read(&mut buf).unwrap();
+
+    let request = String::from_utf8_lossy(&buf[..]).to_string();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: text/html; charset=utf-8\r\n\r\n{}",
+        request.len(),
+        request
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
