4c4e9decc805d80a1859c5a0d84dddbfe8cc9ecd
lzy <zongyu.lang@weimob.com>
2022-07-07 10:59:15 +0800

    添加注释

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
+    // 开启服务并监听7878端口
     let listener = TcpListener::bind("0.0.0.0:7878")?;
 
+    // 循环连接
     for stream in listener.incoming() {
+        // 处理连接
         handle_connection(stream?)
     }
 
@@ -13,18 +16,20 @@ fn main() -> std::io::Result<()> {
 }
 
 fn handle_connection(mut stream: TcpStream) {
+    // 初始化用于从流中读取消息的缓冲区
     let mut buf = [0; 1024];
 
+    // 读取流中消息到缓冲区
     stream.read(&mut buf).unwrap();
 
+    // 将缓冲区消息转为字符串
     let request = String::from_utf8_lossy(&buf[..]).to_string();
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\nContent-Type: text/html; charset=utf-8\r\n\r\n{}",
-        request.len(),
-        request
-    );
+    // 制作消息回复的内容
+    let response = format!("get message: {}", request);
 
+    // 将回复消息写入流缓冲区
     stream.write(response.as_bytes()).unwrap();
+    // 将流写入缓冲区数据刷入响应
     stream.flush().unwrap();
 }
